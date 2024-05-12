e6e2ec4193bd323b662176e3ca471b4601aeb8af
suman <yes.u.man7@gmail.com>
2024-05-12 13:12:21 +0545

    Quick link responsiveness align

--- src/components/TwoCards.jsx
+++ src/components/TwoCards.jsx
@@ -2,9 +2,9 @@ import LineChart from "../components/LineChart";
 
 export function TwoCards() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-wrap justify-center items-center">
       <div className="container mx-auto my-1 ">
-        <div className="grid grid-cols-3 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2  gap-2 max-w-[1400px]">
+        <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2  gap-2 max-w-[1400px]">
           {/* First Card */}
           <div className="col-span-2">
             <CardLayout title="Line Chart">
@@ -12,7 +12,7 @@ export function TwoCards() {
             </CardLayout>
           </div>
           {/* Second Card */}
-          <div className="bg-slate-900 rounded overflow-hidden  shadow-xl m-4 h-[300px] ">
+          <div className=" md:col-span-1 bg-slate-900 rounded overflow-hidden  shadow-xl m-4 h-[300px] ">
             <h3 className="text-2xl font-semibold text-white mb-4">
               <QuickLinks />
             </h3>
@@ -34,11 +34,9 @@ function CardLayout({ title, children }) {
 
 export function QuickLinks() {
   return (
-    <div className=" flex flex-col  justify-between">
+    <div className=" flex flex-col   justify-between">
       <div className="bg-violet-900  rounded hover:bg-violet-600 text-lg  font-medium text-white m-4 h-[40px] ">
-        <a href="#" className="">
-          Buy Coins
-        </a>
+        Buy Coins
       </div>
       <div className="bg-violet-900   hover:bg-violet-600  rounded text-lg font-medium text-white m-4 h-[40px] ">
         Send Coins
